0fefad8ca692ba5e16e7e19dae7a971b936b3fa5
ninjamar <70116773+ninjamar@users.noreply.github.com>
2024-03-12 12:51:50 +0800

    Saves in note app

--- static/projects/note/note.js
+++ static/projects/note/note.js
@@ -1,5 +1,4 @@
-/* import jtmp from "./jtmp.js"; */
-
+// note.js
 
 var templates = {};
 
@@ -28,21 +27,21 @@ function createTemplate(template){
         return $target;
     }
 }
+
 function getAllTemplates(){
     return document.querySelectorAll("[data-template-name]:not([data-template-instance])");
 }
+
 function getAllTemplateInstances(){
     return document.querySelectorAll("[data-template-name][data-template-instance='true'")
 }
+
 // Make sure templates are in `templates`
 function ensureTemplates(){
-    // monkeyPatchEventListener();
-
     for (let $elem of getAllTemplates()){
         templates[$elem.getAttribute("data-template-name")] = createTemplate($elem);
     }
 }
-
 
 // Create a new note
 function newNote(event){
@@ -56,28 +55,53 @@ function newNote(event){
     let $target = templates.note(context);
 
     // Make sure to ensure event listener
+    // Delete should delete element and update storage
     $target.querySelector(".note-card-delete").addEventListener("click", (e) => $target.remove());
     $target.querySelector(".note-card-delete").addEventListener("click", updateSave);
+    // Typing should update storage
     $target.querySelector(".note-card-body").addEventListener("keyup", updateSave)
 
     document.querySelector("#notes").appendChild($target);
+
+    if (event){
+        updateSave();
+    }
+
+    return $target;
 }
 
 function updateSave(){
-    // Get all notes
-    // Record properties of templates
-    // Save these properties to local storage
-    console.log("save updated");
+    let save = [];
+    // Loop through every template
+    for ($elem of getAllTemplateInstances()){
+        let line = {};
+        // Get all template keys, store name and value
+        for ($e of $elem.querySelectorAll("[data-template]")){
+            line[$e.getAttribute("data-template")] = $e.innerHTML;
+        }
+        save.push(line);
+    }
+    localStorage.setItem("notes", JSON.stringify(save));
 }
+
 function loadSave(){
-    // Check if localstorage hs save
-    // If there is a save, load these items into the templates
-    // If no save, load normally
+    let save = localStorage.getItem("notes");
+    if (save){
+        // For every item in save
+        JSON.parse(save).forEach((item) => {
+            // Create a note note
+            let $target = newNote();
+            for ($elem of $target.querySelectorAll("[data-template]")){
+                $elem.innerHTML = item[$elem.getAttribute("data-template")];
+            }
+        });
+    }
+
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
     ensureTemplates();
-
+    loadSave();
+    // `updateSave` is inside `newNote`
     document.getElementById("new-note-submit").addEventListener("click", newNote);
-    document.getElementById("new-note-submit").addEventListener("click", updateSave);
 });

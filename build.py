8badccaaef03657f9891531217b55ead33ef6d6e
ninjamar <70116773+ninjamar@users.noreply.github.com>
2024-03-21 13:40:22 +0800

    Fix workflow

--- build.py
+++ build.py
@@ -7,12 +7,7 @@ from pathlib import Path
 #  watchman-make -p 'templates/**' --run 'python3 build.py template'
 #  watchman-make -p 'static/**' --run 'python3 build.py static'
 
-
-def chunkify(arr, n):
-    return [arr[i : i + n] for i in range(0, len(arr), n)]
-
-
-def template(is_dev=True):
+def template():
     # Load the data directory into env
     data = {}
     for file in os.listdir("data"):
@@ -21,11 +16,8 @@ def template(is_dev=True):
             with open(path) as f:
                 data[Path(path).stem] = json.load(f)
 
-    # Post-process data
-    # data["projects"] = chunkify(data["projects"], 2)
-
     site = Site.make_site(outpath="./dist", env_globals=data)
-    site.render(use_reloader=is_dev)
+    site.render(use_reloader=False)
 
 def static():
     os.system("cp -r static dist") # should probably 
@@ -38,7 +30,7 @@ if __name__ == "__main__":
 
     if "template" in sys.argv:
         print("Templating")
-        template(True if "dev" in sys.argv else False)
+        template()
     else:
         print("Copying static")
         static()

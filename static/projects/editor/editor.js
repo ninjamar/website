fd314f65b571e64ff1b90de283257ed033bb3de0
ninjamar <70116773+ninjamar@users.noreply.github.com>
2024-03-28 13:59:54 +0800

    Cleaned up code

--- static/projects/editor/editor.js
+++ static/projects/editor/editor.js
@@ -98,162 +98,169 @@ function computeOption(option, text){
     return element;
 }
 
-export class EditorBackend {
+/**
+ * Generate a list of options from an element
+ *
+ * @param {HTMLElement} child - An element to generate options from
+ * @return {Array.<Option>} A list of options
+ */
+function createOptionsFromChild(child){
+    let ret = [];
+    let curr = child;
 
-    constructor(){
-
+    while (curr){
+        ret.push(curr)
+        curr = curr.firstElementChild;
     }
-    /**
-     * Generate a list of options from an element
-     *
-     * @param {HTMLElement} child - An element to generate options from
-     * @return {Array.<Option>} A list of options
-     */
-    createOptionsFromChild(child){
-        let ret = [];
-        let curr = child;
-
-        while (curr){
-            ret.push(curr)
-            curr = curr.firstElementChild;
+    ret = ret.map(x => {
+        // Properties would have to be changed here
+        if (x.style.length > 0){
+            // TODO: Won't work for when there are more than one styles
+            // Get the first style, then get the value for it
+            return Option({ name: x.style[0], value: x.style[x.style[0]]});
         }
-        ret = ret.map(x => {
-            // Properties would have to be changed here
-            if (x.style.length > 0){
-                // TODO: Won't work for when there are more than one styles
-                // Get the first style, then get the value for it
-                return Option({ name: x.style[0], value: x.style[x.style[0]]});
-            }
-            return Option({tagName: x.tagName});
-        });
-        return ret;
-    }
-    /**
-     * Toggle an option
-     *
-     * @param {Array.<Option>} childOptions - An array of options
-     * @param {Option} currOption - The option to toggle
-     * @return {Array.<Option>}
-     */
-    toggleOption(childOptions, currOption){
-
-        // Get a copy of the array
-        if (childOptions.some(x => objectEquals(x, currOption))){
-            childOptions = childOptions.filter(x => !objectEquals(x, currOption));
-        } else {
-            childOptions.push(currOption);
-        }
-        return childOptions;
-
-        // if the option is inside childOptions
-        //  remove it
-        // else
-        //  add it
-    }
-    /**
-     * Recursively compute every option from an array
-     *
-     * @param {Array.<Option>} options - A list of options
-     * @param {string} text - Text of element
-     * @return {HTMLElement} 
-     */
-    computeAll(options, text){
-        // [a, b, c] -> a.b.c
-        if (options.length > 0){
-            let ret = computeOption(options[0]);
-            let curr = ret;
-            let elem;
-
-            for (let option of options.slice(1)){ // We have already computed the first item
-                elem = computeOption(option);
-                curr = curr.appendChild(elem);    
-            }
-            curr.appendChild(document.createTextNode(text));
-            return ret;
-
-        }
-        return computeOption(Option({tagName: "SPAN"}), text);
-    }
-    // Replacement for document exec command
-    /* 
-        if contents.firstElementChild
-
-            For every child of contents.children[0]
-                add element name to array
-
-            If the current style isn't in this array, add it
-            If current style is in this array, remove it
-            Create a new element from this array
-        else
-            Create a new element with only style
-    */
-
-    /**
-     * Toggle a style on an element
-     * 
-     * This function can be called with either 1 or 2 arguments
-     * TODO: Use object destructuring here
-     *
-     * @param {string} [tagName] - The tag to toggle
-     * 
-     * @param {string} [rule] - The rule to toggle
-     * @param {string} [value] - The value for the rule
-     */
-    toggleStyle(){
-        let currOption;
-        if (arguments.length == 1){
-            currOption = Option({tagName: arguments[0]})
-        } else if (arguments.length == 2){
-            currOption = Option({ name: arguments[0], value: arguments[1] })
-        } else {
-            throw new Error("Need 1 or 2 arguments");
-        }
-        
-        let range = window.getSelection().getRangeAt(0);   
-        let contents = range.extractContents();
-        let newContents;
-
-        if (contents.firstElementChild){
-            let childOptions = this.createOptionsFromChild(contents.firstElementChild);
-            let filteredChildren = this.toggleOption(childOptions, currOption);
-            newContents = this.computeAll(filteredChildren, contents.textContent);
-
-        } else {
-            newContents = computeOption(currOption, contents.textContent);
-        }
-        
-        range.insertNode(newContents);
-    }
+        return Option({tagName: x.tagName});
+    });
+    return ret;
 }
-let MENU_HTML = `<div id="editor-context-menu">
-                    <ul>
-                        <li>
-                            <button name="bold">
-                                <i class="ph ph-text-b"></i>
-                            </button>
-                        </li>
-                        <li>
-                            <button name="italic">
-                                <i class="ph ph-text-italic"></i>
-                            </button>
-                        </li>
-                        <li>
-                            <button name="strikethrough">
-                                <i class="ph ph-text-strikethrough"></i>
-                            </button>
-                        </li>
-                        <li>
-                            <button name="underline">
-                                <i class="ph ph-text-underline"></i>
-                            </button>
-                        </li>
-                        <li>
-                            <button name="header-2">
-                                <i class="ph ph-text-h-two"></i>
-                            </button>
-                        </li>
-                    </ul>
-                    </div>`;
+/**
+ * Toggle an option
+ *
+ * @param {Array.<Option>} childOptions - An array of options
+ * @param {Option} currOption - The option to toggle
+ * @return {Array.<Option>}
+ */
+function toggleOption(childOptions, currOption){
+
+    // Get a copy of the array
+    if (childOptions.some(x => objectEquals(x, currOption))){
+        childOptions = childOptions.filter(x => !objectEquals(x, currOption));
+    } else {
+        childOptions.push(currOption);
+    }
+    return childOptions;
+
+    // if the option is inside childOptions
+    //  remove it
+    // else
+    //  add it
+}
+/**
+ * Recursively compute every option from an array
+ *
+ * @param {Array.<Option>} options - A list of options
+ * @param {string} text - Text of element
+ * @return {HTMLElement} 
+ */
+function computeAll(options, text){
+    // [a, b, c] -> a.b.c
+    if (options.length > 0){
+        let ret = computeOption(options[0]);
+        let curr = ret;
+        let elem;
+
+        for (let option of options.slice(1)){ // We have already computed the first item
+            elem = computeOption(option);
+            curr = curr.appendChild(elem);    
+        }
+        curr.appendChild(document.createTextNode(text));
+        return ret;
+
+    }
+    return computeOption(Option({tagName: "SPAN"}), text);
+}
+
+// Replacement for document exec command
+/* 
+    if contents.firstElementChild
+
+        For every child of contents.children[0]
+            add element name to array
+
+        If the current style isn't in this array, add it
+        If current style is in this array, remove it
+        Create a new element from this array
+    else
+        Create a new element with only style
+*/
+/**
+ * Toggle a style on an element
+ * 
+ * This function can be called with either 1 or 2 arguments
+ * TODO: Use object destructuring here
+ *
+ * @param {string} [tagName] - The tag to toggle
+ * 
+ * @param {string} [rule] - The rule to toggle
+ * @param {string} [value] - The value for the rule
+ */
+function toggleStyle(){
+    let currOption;
+    if (arguments.length == 1){
+        currOption = Option({tagName: arguments[0]})
+    } else if (arguments.length == 2){
+        currOption = Option({ name: arguments[0], value: arguments[1] })
+    } else {
+        throw new Error("Need 1 or 2 arguments");
+    }
+    
+    let range = window.getSelection().getRangeAt(0);   
+    let contents = range.extractContents();
+    let newContents;
+
+    if (contents.firstElementChild){
+        let childOptions = createOptionsFromChild(contents.firstElementChild);
+        let filteredChildren = toggleOption(childOptions, currOption);
+        newContents = computeAll(filteredChildren, contents.textContent);
+
+    } else {
+        newContents = computeOption(currOption, contents.textContent);
+    }
+    
+    range.insertNode(newContents);
+}
+
+let menuOptions = {
+    html: `
+        <div id="editor-context-menu">
+            <ul>
+                <li>
+                    <button name="bold">
+                        <i class="ph ph-text-b"></i>
+                    </button>
+                </li>
+                <li>
+                    <button name="italic">
+                        <i class="ph ph-text-italic"></i>
+                    </button>
+                </li>
+                <li>
+                    <button name="strikethrough">
+                        <i class="ph ph-text-strikethrough"></i>
+                    </button>
+                </li>
+                <li>
+                    <button name="underline">
+                        <i class="ph ph-text-underline"></i>
+                    </button>
+                </li>
+                <li>
+                    <button name="header-2">
+                        <i class="ph ph-text-h-two"></i>
+                    </button>
+                </li>
+            </ul>
+        </div>
+    `,
+    // Set event listeners based on the name attribute
+    listeners: {
+        "bold": (() => toggleStyle("B")),
+        "italic": (() => toggleStyle("font-style", "italic")),
+        "strikethrough": (() => toggleStyle("text-decoration-line", "line-through")),
+        "underline": (() => toggleStyle("text-decoration-line", "underline")),
+        "header-2": (() => toggleStyle("H2"))
+    }
+};
 
 let ICON_URL = new URL("https://unpkg.com/@phosphor-icons/web").href;
 let CSS_URL = new URL("./editor.css", import.meta.url).href;
@@ -285,24 +292,15 @@ if (document.readyState == "interactive"){
 }
 
 export default class Editor {
+    
     constructor(element, options){
+
         this.element = element;
-
-        this.backend = new EditorBackend();
-
-        this.menuOptions = {};
-        this.menuOptions.html = MENU_HTML;
-        this.menuOptions.listeners = {
-            "bold": (() => this.backend.toggleStyle("B")),
-            "italic": (() => this.backend.toggleStyle("font-style", "italic")),
-            "strikethrough": (() => this.backend.toggleStyle("text-decoration-line", "line-through")),
-            "underline": (() =>this.backend.toggleStyle("text-decoration-line", "underline")),
-            "header-2": (() => this.backend.toggleStyle("H2"))
-        };
+        this.menuOptions = menuOptions;
 
         this.isMenuShown = false;
-        this.menu = null;
 
+        this.menu = document.querySelector("#editor-context-menu") || null;
         // Check if context menu has been loaded
         if (!document.querySelector("#editor-context-menu")){
             // Load context menu
@@ -313,10 +311,12 @@ export default class Editor {
             // Menu is all the way from the top
             this.menu = document.body.appendChild(div.firstElementChild);
         }
-        // Add event listenrs to context menu
+
+        // Add event listeners to context menu
         for (let type of Object.keys(this.menuOptions.listeners)){
             document.querySelector(`#editor-context-menu > ul > li > button[name='${type}']`).addEventListener("click", this.menuOptions.listeners[type]);
         }
+        // Initialize the context menu
         this.initialize(options);
     }
     /**
@@ -380,5 +380,4 @@ export default class Editor {
             });
         }
     }
-
 }

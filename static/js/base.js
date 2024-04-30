74ece92b747e5a7f327d5f08877930979808cd0d
ninjamar <70116773+ninjamar@users.noreply.github.com>
2024-04-30 14:15:23 +0800

    Added analytics to editor??

--- static/js/base.js
+++
@@ -1,6 +0,0 @@
-(() => {
-    // Don't use analytics on localhost
-    if (new URL(window.location.href).hostname != "localhost"){
-        window.analytics.initialize("https://analytics.ninjamar.workers.dev/v1/info");
-    }
-})();

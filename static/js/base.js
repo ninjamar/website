d0a07ba068c34d453125a0f768dc869fa76e6030
ninjamar <70116773+ninjamar@users.noreply.github.com>
2024-05-13 08:04:13 +0800

    Update to remote

--- static/js/base.js
+++ static/js/base.js
@@ -1,7 +1,7 @@
 /* 
     When I get my GitHub account back, use a CDN
 */
-window.analytics={initialize:function(e){let t,i=crypto.randomUUID();t=setTimeout((function(){clearTimeout(t);let n=new Date;document.addEventListener("visibilitychange",(()=>{"hidden"==document.visibilityState?function(t){let n=new URL(window.location.href);fetch(e,{body:JSON.stringify({id:i,totalTimeOnPage:t,currentTime:(new Date).getTime(),width:document.body.clientWidth,height:document.body.clientHeight,timezone:Intl.DateTimeFormat().resolvedOptions().timeZone,userAgent:navigator.userAgent,isMobile:navigator.userAgentData.mobile,isTouchScreen:navigator.maxTouchPoints>0,referrer:document.referrer||null,host:n.hostname,port:n.port||null,path:n.pathname}),method:"POST",keepalive:!0,mode:"no-cors"})}(new Date-n):"visible"==document.visibilityState&&(n=new Date)}))}),0)}};
+window.analytics={initialize:function(e){let t;crypto.randomUUID(),t=setTimeout((function(){clearTimeout(t);let i=new Date;document.addEventListener("visibilitychange",(()=>{"hidden"==document.visibilityState?function(t){let i=new URL(window.location.href);fetch(e,{body:JSON.stringify({totalTimeOnPage:t,currentTime:(new Date).getTime(),width:document.body.clientWidth,height:document.body.clientHeight,timezone:Intl.DateTimeFormat().resolvedOptions().timeZone,userAgent:navigator.userAgent,isMobile:navigator.userAgentData.mobile,isTouchScreen:navigator.maxTouchPoints>0,referrer:document.referrer||null,host:i.hostname,port:i.port||null,path:i.pathname}),method:"POST",keepalive:!0,mode:"no-cors"})}(new Date-i):"visible"==document.visibilityState&&(i=new Date)}))}),2e3)}};
 
 (() => {
     // Don't use analytics on localhost

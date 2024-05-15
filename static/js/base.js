73b56c435e94b1bbf06bdb2edd420669088036b4
ninjamar <70116773+ninjamar@users.noreply.github.com>
2024-05-15 09:55:11 +0800

    Update submodules

--- static/js/base.js
+++ static/js/base.js
@@ -2,6 +2,6 @@
 (() => {
     // Don't use analytics on localhost
     if (new URL(window.location.href).hostname != "localhost"){
-        window.analytics.initialize("https://analytics.ninjamar.workers.dev/v1/info");
+        window.analytics.initialize("https://analytics.ninjamar.dev/v1/info");
     }
 })();

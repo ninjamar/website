b2b13858878ecb338c1513013896f02d1591c285
ninjamar <ninjamar@users.noreply.github.com>
2024-05-15 01:29:46 +0000

    deploy: ee093ba0d9b50a9655ecd2c133ca5de5ce170012

--- static/js/base.js
+++ static/js/base.js
@@ -1,8 +1,4 @@
-/* 
-    When I get my GitHub account back, use a CDN
-*/
-window.analytics={initialize:function(e){let t;crypto.randomUUID(),t=setTimeout((function(){clearTimeout(t);let i=new Date;document.addEventListener("visibilitychange",(()=>{"hidden"==document.visibilityState?function(t){let i=new URL(window.location.href);fetch(e,{body:JSON.stringify({totalTimeOnPage:t,currentTime:(new Date).getTime(),width:document.body.clientWidth,height:document.body.clientHeight,timezone:Intl.DateTimeFormat().resolvedOptions().timeZone,userAgent:navigator.userAgent,isMobile:navigator.userAgentData.mobile,isTouchScreen:navigator.maxTouchPoints>0,referrer:document.referrer||null,host:i.hostname,port:i.port||null,path:i.pathname}),method:"POST",keepalive:!0,mode:"no-cors"})}(new Date-i):"visible"==document.visibilityState&&(i=new Date)}))}),2e3)}};
-
+// CDN @ https://cdn.jsdelivr.net/gh/ninjamar/analytics@latest/analytics.min.js
 (() => {
     // Don't use analytics on localhost
     if (new URL(window.location.href).hostname != "localhost"){

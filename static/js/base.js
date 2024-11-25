832b5600ff750f659dde34e32bd05a67c85c9d5a
ninjamar <70116773+ninjamar@users.noreply.github.com>
2024-11-25 12:00:41 +0800

    self initializing analytics script

--- static/js/base.js
+++ static/js/base.js
@@ -1,7 +1,18 @@
 // CDN @ https://cdn.jsdelivr.net/gh/ninjamar/analytics@latest/analytics.min.js
-(() => {
+(function(){
+    function loadFromURL(url, onload){
+        let tag = document.createElement("script");
+        tag.src = url;
+        tag.onload = onload;
+        document.head.appendChild(tag);
+    }
+    let setupAnalytics = () => window.analytics.initialize("https://analytics.ninjamar.dev/v1/info");
     // Don't use analytics on localhost
     if (new URL(window.location.href).hostname != "localhost"){
-        window.analytics.initialize("https://analytics.ninjamar.dev/v1/info");
+        if (!window.analytics){
+            loadFromURL("https://cdn.jsdelivr.net/gh/ninjamar/analytics@latest/analytics.min.js", setupAnalytics);
+        } else {
+            setupAnalytics();
+        }
     }
-})();
+})()

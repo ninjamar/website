a8887cb0c5c3c428c301c024e498bf0f7c0a102d
ninjamar <70116773+ninjamar@users.noreply.github.com>
2026-01-16 13:26:44 +0800

    add css-grid-lanes polyfill

--- root.js
+++ root.js
@@ -1,15 +1,3 @@
-function main(){
-    const grid = document.querySelector('.grid');
-    const msnry = new Masonry(grid, {
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-item',
-
-        percentPosition: true
-    });
-
-    imagesLoaded(grid).on('progress', function() {
-        msnry.layout();
-    });
-}
-
-// document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', function () {
+    GridLanesPolyfill.init({ force: true });
+});

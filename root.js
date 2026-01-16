547cbbe33b804dd90c99b5425b238792c54538ca
ninjamar <70116773+ninjamar@users.noreply.github.com>
2026-01-16 13:31:49 +0800

    only apply polyfill if native grid lanes is not supported

--- root.js
+++ root.js
@@ -1,3 +1,11 @@
+
 document.addEventListener('DOMContentLoaded', function () {
-    GridLanesPolyfill.init({ force: true });
+    // https://github.com/simonw/tools/commit/8ca341f1fa7f535682f4cf4271669c99ef0befbb
+    // TODO: Use resizeobserver and resize grid lanes on refresh
+    if (GridLanesPolyfill.supportsGridLanes()){
+        console.log("Using native grid lanes;")
+    } else {
+        console.log("Using grid-lanes polyfill");
+        GridLanesPolyfill.init({ force: true });
+    }
 });

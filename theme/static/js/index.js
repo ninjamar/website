4be0f142ea33a769bacce3d24caa044a1993af4c
ninjamar <70116773+ninjamar@users.noreply.github.com>
2026-03-14 22:44:03 +0800

    Add two column layout on mobile

--- theme/static/js/index.js
+++ theme/static/js/index.js
@@ -1,30 +1,24 @@
+import GridLanesPolyfill from "https://cdn.jsdelivr.net/gh/ninjamar/grid-lanes-polyfill@v1.0.0/grid-lanes-polyfill.js";
+
 document.addEventListener("DOMContentLoaded", () => {
+
+    if (typeof GLightbox !== "undefined") {
+        GLightbox({ selector: ".photo-lightbox" });
+    }
 
     const supportsGridLanes = GridLanesPolyfill.supportsGridLanes();
 
-    // check css variable and use resize observer
     let size = parseFloat(window.getComputedStyle(document.documentElement).getPropertyValue("--mobile-min-width"));
 
     const mediaQuery = window.matchMedia(`(max-width: ${size}px)`);
-    
-    let options = {};
-    if (supportsGridLanes){
-        console.log("Using native grid lanes");
-    } else {
+
+    if (!supportsGridLanes) {
         console.log("Using grid lanes polyfill");
     }
 
-    function updateGridLanes(isMobile){
-        if (supportsGridLanes) return;
-        if (isMobile){
-            if (options?.destroy) options.destroy();
-        } else {
-            options = GridLanesPolyfill.init({ force: true });
-        }
-    }
-    const lightbox = GLightbox({ selector: ".photo-lightbox" });
 
-
+    const options = supportsGridLanes? {} : GridLanesPolyfill.init({ force: true });
+    window.options = options
     function updateMobileClass(e) {
         const isMobile = e.matches; // css query matches
         if (isMobile) {
@@ -32,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             document.body.classList.remove("mobile");
         }
-        updateGridLanes(isMobile);
+        options?.refresh();
     }
 
     updateMobileClass(mediaQuery); // run once on load
